85ff6791b0919c4595ee4a36dff3e9bb7b710c43
nuraansa <nuraansa@gmail.com>
2023-05-17 09:46:25 +0200

    First Commit

---
+++ script/code.js
@@ -0,0 +1,19 @@
+//Q1
+let me = "Nuraan"
+console.log(typeof me)
+
+//Q2
+let me = 23
+console.log(typeof me)
+
+//Q3
+let firstName = "Nuraan";
+let age = 23;
+let married = true;
+let surname;
+let favSong = null;
+console.log(typeof firstName); //string
+console.log(typeof age); //number
+console.log(typeof married); //boolean
+console.log(typeof surname); //undefined
+console.log(typeof favSong); //null/object
